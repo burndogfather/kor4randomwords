ac0778b8c3b5b612dead048d09684f345193c943
coco <squarehacker@kakao.com>
2023-03-31 04:47:32 +0900

    2023-03-31 04:47:32 업데이트!!!

---
+++ sample.js
@@ -0,0 +1,19 @@
+
+fetch('https://cdn.jsdelivr.net/gh/squarehacker/kor4randomwords/resource/dataset.json')
+.then((response) => response.json())
+.then(function(data){
+	
+	let maxAdjective = Object.keys(data.adjective).length;
+	let maxNoun = Object.keys(data.noun).length;
+	let lengthNoun = maxNoun.length;
+	let randAdjective, randNoun;
+	
+	setInterval(function(){ 
+		
+		randAdjective = Math.floor(Math.random() * maxAdjective);
+		randNoun = Math.floor(Math.random() * maxNoun);
+		
+		document.querySelector('h1').innerText = data.adjective[randAdjective]+data.noun[randNoun];
+		
+	},70);
+});
